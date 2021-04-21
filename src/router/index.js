d7539545f044bbe61a6a47d259b91e6ea272bcc1
Ramdeep Hunjan <ramdeep195@gmail.com>
2021-04-21 15:54:51 +0200

    adding 404 page, event ID dynamic function

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import EventShow from '../views/EventShow.vue'
 import EventList from '../views/EventList.vue'
 import EventCreate from '../views/EventCreate.vue'
+import User from '../views/User.vue'
+import NotFoundComponent from '../views/NotFoundComponent.vue'
 
 Vue.use(VueRouter)
 
@@ -13,15 +15,24 @@ const routes = [
     component: EventList,
   },
   {
-    path: '/event',
+    path: '/event/:id',
     name: 'event-show',
     component: EventShow,
+    props:true
   },
   {
     path: '/event-create',
     name: 'event-create',
     component: EventCreate,
   },
+  {
+    path: '/user/:username',
+    name: 'user',
+    component: User,
+    props: true
+  },
+  { path: '*', 
+    component: NotFoundComponent }
 ]
 
 const router = new VueRouter({
